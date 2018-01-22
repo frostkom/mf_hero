8b24e3c0ffb9a9d465e243bd0768924229dd8d27
frostkom <martin.fors@frostkom.se>
2018-01-22 14:37:55 +0100

    Add licence

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 Plugin Name: MF Hero
 Plugin URI: https://github.com/frostkom/mf_hero
 Description: 
-Version: 2.2.1
+Version: 2.2.2
+Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: http://frostkom.se
 Text Domain: lang_hero
