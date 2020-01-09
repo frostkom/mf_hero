5d6de5851c618ff5287a88d3afaa1a3777d19481
frostkom <martin.fors@frostkom.se>
2020-01-09 14:56:42 +0100

    Find files used in meta

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Hero
 Plugin URI: https://github.com/frostkom/mf_hero
 Description: 
-Version: 2.2.22
+Version: 2.2.23
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se
@@ -32,6 +32,8 @@ else
 	add_filter('is_active_sidebar', array($obj_hero, 'is_active_sidebar'), 10, 2);
 	add_action('dynamic_sidebar_after', array($obj_hero, 'dynamic_sidebar_after'));
 }
+
+add_filter('filter_is_file_used', array($obj_hero, 'filter_is_file_used'));
 
 add_action('widgets_init', array($obj_hero, 'widgets_init'));
 
