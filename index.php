5f94fcbb383b1794af9152c3a9e2c511b2980186
Martin Fors <martin.fors@frostkom.se>
2025-07-20 11:01:30 +0200

    Change how required plugins are checked

--- index.php
+++ index.php
@@ -3,12 +3,14 @@
 Plugin Name: MF Hero
 Plugin URI: https://github.com/frostkom/mf_hero
 Description:
-Version: 2.6.3
+Version: 2.6.4
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://martinfors.se
 Text Domain: lang_hero
 Domain Path: /lang
+
+Requires Plugins: meta-box
 */
 
 if(!function_exists('is_plugin_active') || function_exists('is_plugin_active') && is_plugin_active("mf_base/index.php") && is_plugin_active("mf_theme_core/index.php"))
@@ -17,15 +19,11 @@ if(!function_exists('is_plugin_active') || function_exists('is_plugin_active') &
 
 	$obj_hero = new mf_hero();
 
-	add_action('cron_base', 'activate_hero', mt_rand(1, 10));
-
 	add_action('enqueue_block_editor_assets', array($obj_hero, 'enqueue_block_editor_assets'));
 	add_action('init', array($obj_hero, 'init'));
 
 	if(is_admin())
 	{
-		register_activation_hook(__FILE__, 'activate_hero');
-
 		add_filter('display_post_states', array($obj_hero, 'display_post_states'), 10, 2);
 
 		add_action('rwmb_meta_boxes', array($obj_hero, 'rwmb_meta_boxes'));
@@ -41,9 +39,4 @@ if(!function_exists('is_plugin_active') || function_exists('is_plugin_active') &
 	add_filter('filter_is_file_used', array($obj_hero, 'filter_is_file_used'));
 
 	add_action('widgets_init', array($obj_hero, 'widgets_init'));
-
-	function activate_hero()
-	{
-		require_plugin("meta-box/meta-box.php", "Meta Box");
-	}
 }
