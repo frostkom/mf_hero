0d908f25ce6b263c93f611a8ce9a8cd87345eca1
frostkom <martin.fors@frostkom.se>
2025-10-20 06:11:33 +0200

    Remove @media all

--- include/style.php
+++ include/style.php
@@ -29,201 +29,196 @@ if(!isset($setting_hero_bg_color))
 	$setting_hero_bg_color = "rgba(255, 255, 255, 0)";
 }
 
-echo "@media all
-{";
-
-	if($setting_hero_bg_color != '')
+if($setting_hero_bg_color != '')
+{
+	echo ".widget.hero.allow_bg_color
 	{
-		echo ".widget.hero.allow_bg_color
-		{
-			background: ".$setting_hero_bg_color.";
-		}";
+		background: ".$setting_hero_bg_color.";
+	}";
+}
+
+	echo "#wrapper .widget.hero > div, .wp-site-blocks .widget.hero > div
+	{
+		overflow: hidden;
+		padding-top: 0;
+		padding-bottom: 0;
 	}
 
-		echo "#wrapper .widget.hero > div, .wp-site-blocks .widget.hero > div
+		.widget.hero .align_center
 		{
-			overflow: hidden;
-			padding-top: 0;
-			padding-bottom: 0;
+			text-align: center;
 		}
 
-			.widget.hero .align_center
+			.widget.hero .align_center img, .widget.hero .align_ontop img
 			{
-				text-align: center;
+				width: 100%;
 			}
 
-				.widget.hero .align_center img, .widget.hero .align_ontop img
+		.widget.hero .image
+		{
+			display: block;
+			position: relative;
+		}
+
+			.widget.hero .align_ontop .image + .content_container
+			{
+				left: 50%;
+				position: absolute;
+				top: 50%;
+				transform: translate(-50%, -50%);
+				width: 60%;
+			}
+
+			.widget.hero .align_right .image, .widget.hero .align_left .image
+			{
+				width: 59%;
+			}
+
+			.widget.hero .align_right.has_text
+			{
+				padding-right: 0;
+			}
+
+				.widget.hero .align_right .image
 				{
+					float: right;
+				}
+
+			.widget.hero .align_left.has_text
+			{
+				padding-left: 0;
+			}
+
+				.widget.hero .align_left .image
+				{
+					float: left;
+				}";
+
+			if($setting_hero_bg_color != '')
+			{
+				echo ".widget.hero .image.image_fade:before, .widget.hero .image.image_fade:after
+				{
+					bottom: 0;
+					content: '';
+					position: absolute;
+					top: 0;
+					width: 20%;
+				}
+
+					.widget.hero .image.image_fade:before
+					{
+						background: linear-gradient(to right, ".$setting_hero_bg_color." 0, transparent 100%);
+						left: 0;
+					}
+
+					.widget.hero .image.image_fade:after
+					{
+						background: linear-gradient(to left, ".$setting_hero_bg_color." 0, transparent 100%);
+						right: 0;
+					}
+
+						.is_mobile .widget.hero .image.image_fade
+						{
+							float: none;
+							margin-bottom: -10%;
+							text-align: center;
+							width: 100%;
+						}
+
+				.widget.hero .image.image_fade div:after
+				{
+					background: linear-gradient(to top, ".$setting_hero_bg_color." 0, transparent 100%);
+					bottom: 0;
+					content: '';
+					left: 0;
+					position: absolute;
+					right: 0;
+					height: 20%;
+				}
+
+				.widget.hero .image.image_solid div img
+				{
+					filter: grayscale(1);
 					width: 100%;
 				}
 
-			.widget.hero .image
-			{
-				display: block;
-				position: relative;
+				.widget.hero .image.image_solid div:after
+				{
+					background-color: ".$setting_hero_bg_color.";
+					bottom: 0;
+					content: '';
+					left: 0;
+					opacity: .7;
+					position: absolute;
+					right: 0;
+					top: 0;
+				}";
 			}
 
-				.widget.hero .align_ontop .image + .content_container
+				echo ".widget.hero .image img
 				{
-					left: 50%;
-					position: absolute;
-					top: 50%;
-					transform: translate(-50%, -50%);
-					width: 60%;
+					display: block;
+					transition: all 1s ease;
 				}
 
-				.widget.hero .align_right .image, .widget.hero .align_left .image
-				{
-					width: 59%;
-				}
-
-				.widget.hero .align_right.has_text
-				{
-					padding-right: 0;
-				}
-
-					.widget.hero .align_right .image
+					.widget.hero:hover .image img
 					{
-						float: right;
+						transform: scale(1.05);
 					}
 
-				.widget.hero .align_left.has_text
+				.widget.hero h3, .widget.hero .content
 				{
-					padding-left: 0;
+					position: relative;
 				}
 
-					.widget.hero .align_left .image
+					.widget.hero .align_right h3, .widget.hero .align_right .content, .widget.hero .align_left h3, .widget.hero .align_left .content
 					{
+						clear: left;
 						float: left;
-					}";
-
-				if($setting_hero_bg_color != '')
-				{
-					echo ".widget.hero .image.image_fade:before, .widget.hero .image.image_fade:after
-					{
-						bottom: 0;
-						content: '';
-						position: absolute;
-						top: 0;
-						width: 20%;
+						width: 50%;
 					}
 
-						.widget.hero .image.image_fade:before
-						{
-							background: linear-gradient(to right, ".$setting_hero_bg_color." 0, transparent 100%);
-							left: 0;
-						}
-
-						.widget.hero .image.image_fade:after
-						{
-							background: linear-gradient(to left, ".$setting_hero_bg_color." 0, transparent 100%);
-							right: 0;
-						}
-
-							.is_mobile .widget.hero .image.image_fade
-							{
-								float: none;
-								margin-bottom: -10%;
-								text-align: center;
-								width: 100%;
-							}
-
-					.widget.hero .image.image_fade div:after
+					.widget.hero .align_right h3, .widget.hero .align_right .content
 					{
-						background: linear-gradient(to top, ".$setting_hero_bg_color." 0, transparent 100%);
-						bottom: 0;
-						content: '';
-						left: 0;
-						position: absolute;
-						right: 0;
-						height: 20%;
+						margin-right: -10% !important;
 					}
 
-					.widget.hero .image.image_solid div img
+					.widget.hero .align_left h3, .widget.hero .align_left .content
 					{
-						filter: grayscale(1);
-						width: 100%;
+						margin-left: -10% !important;
+						text-align: right;
 					}
 
-					.widget.hero .image.image_solid div:after
+						.is_mobile .widget.hero h3, .is_mobile .widget.hero .content
+						{
+							float: none;
+							margin: 0;
+							text-align: center;
+							width: 100%;
+						}";
+
+					if(class_exists('mf_theme_core'))
 					{
-						background-color: ".$setting_hero_bg_color.";
-						bottom: 0;
-						content: '';
-						left: 0;
-						opacity: .7;
-						position: absolute;
-						right: 0;
-						top: 0;
-					}";
-				}
+						echo ".widget.hero h3
+						{"
+							.$obj_theme_core->render_css(array('property' => 'font-size', 'value' => 'hero_h3_size', 'suffix' => ' !important'))
+							.$obj_theme_core->render_css(array('property' => 'margin-top', 'value' => 'hero_h3_margin_top', 'suffix' => ' !important'))
+						."}
 
-					echo ".widget.hero .image img
-					{
-						display: block;
-						transition: all 1s ease;
-					}
-
-						.widget.hero:hover .image img
-						{
-							transform: scale(1.05);
-						}
-
-					.widget.hero h3, .widget.hero .content
-					{
-						position: relative;
-					}
-
-						.widget.hero .align_right h3, .widget.hero .align_right .content, .widget.hero .align_left h3, .widget.hero .align_left .content
-						{
-							clear: left;
-							float: left;
-							width: 50%;
-						}
-
-						.widget.hero .align_right h3, .widget.hero .align_right .content
-						{
-							margin-right: -10% !important;
-						}
-
-						.widget.hero .align_left h3, .widget.hero .align_left .content
-						{
-							margin-left: -10% !important;
-							text-align: right;
-						}
-
-							.is_mobile .widget.hero h3, .is_mobile .widget.hero .content
-							{
-								float: none;
-								margin: 0;
-								text-align: center;
-								width: 100%;
-							}";
-
-						if(class_exists('mf_theme_core'))
-						{
-							echo ".widget.hero h3
+							.is_mobile .widget.hero h3
 							{"
-								.$obj_theme_core->render_css(array('property' => 'font-size', 'value' => 'hero_h3_size', 'suffix' => ' !important'))
-								.$obj_theme_core->render_css(array('property' => 'margin-top', 'value' => 'hero_h3_margin_top', 'suffix' => ' !important'))
+								.$obj_theme_core->render_css(array('property' => 'font-size', 'value' => 'hero_h3_size_mobile', 'suffix' => ' !important'))
 							."}
 
-								.is_mobile .widget.hero h3
-								{"
-									.$obj_theme_core->render_css(array('property' => 'font-size', 'value' => 'hero_h3_size_mobile', 'suffix' => ' !important'))
-								."}
+						.widget.hero .content p
+						{"
+							.$obj_theme_core->render_css(array('property' => 'font-size', 'value' => 'hero_content_size', 'suffix' => ' !important'))
+							.$obj_theme_core->render_css(array('property' => 'padding', 'value' => 'hero_content_padding'))
+						."}
 
-							.widget.hero .content p
+							.is_mobile .widget.hero .content p
 							{"
-								.$obj_theme_core->render_css(array('property' => 'font-size', 'value' => 'hero_content_size', 'suffix' => ' !important'))
-								.$obj_theme_core->render_css(array('property' => 'padding', 'value' => 'hero_content_padding'))
-							."}
-
-								.is_mobile .widget.hero .content p
-								{"
-									.$obj_theme_core->render_css(array('property' => 'font-size', 'value' => 'hero_content_size_mobile', 'suffix' => ' !important'))
-									."margin-top: 2vw !important;"
-								."}";
-						}
-
-echo "}";
+								.$obj_theme_core->render_css(array('property' => 'font-size', 'value' => 'hero_content_size_mobile', 'suffix' => ' !important'))
+								."margin-top: 2vw !important;"
+							."}";
+					}
